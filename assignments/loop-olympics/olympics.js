efea7dcdf56ac030fa121713e4fd86ec98065029
Ally <allyjnickell@gmail.com>
2020-12-18 21:55:13 -0700

    add loop olympics

---
+++ assignments/loop-olympics/olympics.js
@@ -0,0 +1,35 @@
+
+
+for (var i = 0; i <= 10; i++) {
+    console.log(i);
+}
+
+for (var i = 0; i >= -10; i--) {
+    console.log(i);
+}
+
+var fruit = ["banana", "orange", "apple", "kiwi"]
+
+for (var i = 0; i < fruit.length; i++) {
+    console.log(fruit[i]);
+}
+
+var newArray = [];
+
+for (var i = 0; i < 10; i++) {
+    newArray.push(i)
+}
+console.log(newArray)
+
+for (var i = 0; i < 100; i++) {
+    console.log(i++)
+}
+
+var fruit = ["banana", "orange", "apple", "kiwi", "pear", "peach"]
+
+for (var i = 0; i < fruit.length; i++) {
+    console.log(fruit[i++])
+}
+
+
+
