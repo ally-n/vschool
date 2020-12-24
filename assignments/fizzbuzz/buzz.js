d8dc21e66b99ba284e9a98a674fbe6f5a9482a90
Ally Nickell <allyjnickell@gmail.com>
2020-12-23 22:42:11 -0700

    adding FizzBuzz

---
+++ assignments/fizzbuzz/buzz.js
@@ -0,0 +1,17 @@
+for (var i = 0; i <= 100; i++) {
+    if(i % 3 && i % 5 ) {
+        console.log(i);
+    } else {
+        if(i % 3 == 0 ) {
+            console.log("Fizz");
+        } if (i % 5 == 0 ) {
+            console.log("Buzz");
+        } if (i % 3 && i % 5 ) {
+            console.log(i)
+        } if (i % 3 == 0 && i % 5 == 0) {
+            console.log("FizzBuzz");
+        }
+       
+
+    }
+}
