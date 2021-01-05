11e6060369549407afca7e7bd4edc9eb5b32aef8
Ally Nickell <allyjnickell@gmail.com>
2021-01-04 21:53:21 -0700

    Fixed battle function bracket on RPG

--- assignments/adventure-rpg/main.js
+++ assignments/adventure-rpg/main.js
@@ -24,6 +24,7 @@ const userName = readlineSync.question('I heard you have a nickname you want us 
 console.log('Hi ' + userName + '! Let us begin.');
 console.log('Shall we see what miscreants stand in our journey to the Seeing Stone? ')
 explore()
+
 function explore () {
     while (!gameOver) {
         readlineSync.setDefaultOptions({limit: ['w', 'p', 'print', 'q']})
@@ -42,6 +43,7 @@ function explore () {
         }
     }
 }
+
 function enemyAppears () {
     let num = Math.floor(Math.random()*4)
     if (num === 3) {
@@ -50,10 +52,9 @@ function enemyAppears () {
         return false;
     }
 }
+
 function battle () {
     let randomEnemy = enemies[Math.floor(Math.random()*enemies.length)];
-    let attack = Math.floor(Math.random() * (randomEnemy.maxDamage - randomEnemy.minDamage)) + randomEnemy.minDamage;
-    let enemyAttack = Math.floor(Math.random()* (randomEnemy.maxDamage - randomEnemy.minDamage));
     console.log('Beware- it is ' + randomEnemy.enemyName + '!')
             while (userHealth >= 0 || randomEnemy.HP >= 0) {
             readlineSync.setDefaultOptions({limit: ['f', 'r', 'q']})  
@@ -65,9 +66,12 @@ function battle () {
             } else if (runFight === 'r') {
                 run()
         }
+    }
 }
 
 function fight(obj) {
+    let attack = Math.floor(Math.random() * (obj.maxDamage - obj.minDamage)) + obj.minDamage;
+    let enemyAttack = Math.floor(Math.random()* (obj.maxDamage - obj.minDamage));
     console.log('You use the force! ');
     obj.HP = obj.HP - attack
     console.log('You dealt ' + attack + ' points of damage! ' + obj.enemyName + ' currently has ' + obj.HP+ ' health points left.')
@@ -83,12 +87,13 @@ function fight(obj) {
     console.log('Watch out for an attack! Oh no- ' + obj.enemyName + ' dealt ' + enemyAttack + ' points of damage to you! You currently have ' + userHealth + ' health points left.')
     if (userHealth <= 0) {
         console.log('You have died. The force can\'t save you now. BYE. ')
-        gameOver === true
+        gameOver = true
     }
     if (enemies.length = 0) {
-        hasWon === true;
+        hasWon = true;
     }
 }
+
 function run() {
     let escape = Math.floor(Math.random()*2)
     if (escape === 1) {
@@ -97,19 +102,22 @@ function run() {
         console.log('Oh no,' + userName + ', you weren\'t quite fast enough. They\'re about to attack! Are you going to defend yourself?');
     }   
 }
+
 function quit() {
     console.log('May the force be with you. ')
-    gameOver === true;
+    gameOver = true;
 }
+
 if (gameOver === true) {
     console.log('HEY LOSER- I CAN\'T BELIEVE YOU LOST ')
     console.log('GAME OVER')
 }
+
 if (hasWon === true) {
     console.log('We\'ve arrived to the Seeing Stone! Congratulations. I wish you luck on your adventure.')
     console.log('GAME OVER')
 }
-}
+
 
 // Issues
 // Lines 61-62 in Battle Function doesn't quit/End Game like it does in the Explore Function (lines 31-33- but those lines ONLY work the first time it runs. Not after you finish a battle loop and go back to the explore loop.)
