77782142229da1432b34793a38ce672ee99787da
Ally Nickell <allyjnickell@gmail.com>
2021-02-27 14:35:24 -0700

    json's deli completed

--- assignments/speed-clicker/main.js
+++ assignments/speed-clicker/main.js
@@ -1,39 +1,15 @@
-var img = document.getElementById("kitty")
-img.addEventListener("click", clickCount())
-var count = 0
-// if (localStorage.getItem("clickStorage") === null) {
-//     localStorage.setItem(document.getElementById("clicks").innerHTML = 0)
-// }
-if (localStorage.getItem("clickStorage")){
-    count=localStorage.getItem("clickStorage")
-  } else {
-      count = 0
-  }
-function clickCount() {
-    count += 1;
-    document.getElementById("clicks").innerHTML = count
-    console.log(count) 
-    localStorage.setItem("clickStorage", count)
+let img = document.getElementById("kitty")
+img.addEventListener("click", clickCount)
+let count = 0
+localStorage.getItem("numberOfClicks")       
+if ("numberOfClicks" === 0) {              
+    document.getElementById("clicks").innerHTML = 0
+} else {                               
+    document.getElementById("clicks").innerHTML = localStorage.getItem("numberOfClicks")   
 }
-localStorage.setItem("clickStorage", count)
-localStorage.getItem("clickStorage") 
-
-
-
-// let bodyClick = document.getElementById("clickme")
-// let par=document.getElementById("paragraph")
-// let count = 0;
-
-// if (localStorage.getItem("numberOfClicks")){
-//   count=localStorage.getItem("numberOfClicks")
-// }  
-// else {count=0}
-
-// function event(){
-//   count ++
-//   par.textContent = count
-//   localStorage.setItem("numberOfClicks", count)
-// }
-// localStorage.setItem("numberOfClicks", count)
-
-// bodyClick.addEventListener("click", event)
+function clickCount() {
+    count += 1
+    localStorage.setItem("numberOfClicks", count)
+    document.getElementById("clicks").innerHTML = localStorage.getItem("numberOfClicks")
+    console.log(count)
+}
