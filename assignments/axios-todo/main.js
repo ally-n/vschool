bc4e4b09463c07069188f02495d38ada9c0f1037
Ally Nickell <allyjnickell@gmail.com>
2021-03-06 09:10:01 -0700

    axios todo- working post

--- assignments/axios-todo/main.js
+++ assignments/axios-todo/main.js
@@ -3,62 +3,62 @@
 //         .then(res => listData(res.data))
 //         .catch(err => console.log(err))
 // }
-    axios.get("https://api.vschool.io/[ally_n]/todo")
-        .then(response => {
-            for(let i = 0; i < response.data.length; i++){
-                const h1 = document.createElement('h1')
-                const h2 = document.createElement('p')
-                const p = document.createElement('p')
-                const img = document.createElement('img')
-                const editBtn = document.createElement('BUTTON')
-                const delBtn = document.createElement('BUTTON')
-                var x = document.createElement("INPUT");
-                x.className = 'checkbox';
-                var label = document.createElement("label");
-                x.setAttribute("type", "checkbox")
-                h1.textContent = response.data[i].title
-                h2.textContent = response.data[i].price
-                p.textContent = response.data[i].description
-                img.src = response.data[i].imgUrl
-                img.width = 300
-                editBtn.textContent = "Edit"
-                delBtn.textContent = "Delete"
-                document.getElementById('todo-list').style.padding.bottom = "100px";
-                document.getElementById('todo-list').appendChild(h1)
-                document.getElementById('todo-list').appendChild(h2)
-                document.getElementById('todo-list').appendChild(p)
-                document.getElementById('todo-list').appendChild(x)
-                document.getElementById('todo-list').appendChild(label)
-                document.getElementById('todo-list').appendChild(delBtn)
-                document.getElementById('todo-list').appendChild(img)
-                document.getElementById('todo-list').appendChild(editBtn)
+
+//NEED TO WORK ON
+//Format and organization
+//edit and delete buttons working
+
+
+axios.get("https://api.vschool.io/[ally_n]/todo")
+    .then(response => {
+        for(let i = 0; i < response.data.length; i++){
+            const h1 = document.createElement('h1')
+            const h2 = document.createElement('p')
+            const p = document.createElement('p')
+            const img = document.createElement('img')
+            const editBtn = document.createElement('BUTTON')
+            const delBtn = document.createElement('BUTTON')
+            var x = document.createElement("INPUT");
+            x.className = 'checkbox';
+            var label = document.createElement("label");
+            label.textContent = "Completed?"
+            x.setAttribute("type", "checkbox")
+            x.setAttribute("name", "completed")
+            h1.textContent = response.data[i].title
+            h2.textContent = response.data[i].price
+            p.textContent = response.data[i].description
+            img.src = response.data[i].imgUrl
+            img.width = 300
+            editBtn.textContent = "Edit"
+            delBtn.textContent = "Delete"
+            document.getElementById('todo-list').style.padding.bottom = "100px";
+            document.getElementById('todo-list').appendChild(h1)
+            document.getElementById('todo-list').appendChild(h2)
+            document.getElementById('todo-list').appendChild(p)
+            document.getElementById('todo-list').appendChild(x)
+            document.getElementById('todo-list').appendChild(label)
+            document.getElementById('todo-list').appendChild(delBtn)
+            document.getElementById('todo-list').appendChild(img)
+            document.getElementById('todo-list').appendChild(editBtn)
+
+        //add edit and delete buttons
+        //add text ` Check here once complete!` on checkbox  
         }
     })
     .catch(error => console.log(error))
 
-    delBtn.addEventListener("click", function(){
-        axios.delete("https://api.vschool.io/[ally_n]/todo" + object._id)
-            .then(res => getData())
-            .catch(error => console.log(error))
-    })
+    var deleteTodo = function(singleTodoObject) {
+        // This "singleTodoObject" I passed in has an attribute "_id" I can use to delete it
+        // I just need to add that "_id" to the end of my URL to which I'm sending this DELETE request
+            axios.delete("https://api.vschool.io/jonsmith/todo/" + singleTodoObject._id).then(function(response) {
+                // This made a DELETE request to "https://api.vschool.io/jonsmith/todo/5630dcfcac2dfab2428b8c02"
+                // Assuming I used the object from the example above.
+                alert("Your todo was successfully deleted!")
+            }, function(response) {
+                alert("There was a problem deleting your todo :(");
+            });
+        };
 
-    // var deleteTodo = function(singleTodoObject) {
-    //     // This "singleTodoObject" I passed in has an attribute "_id" I can use to delete it
-    //     // I just need to add that "_id" to the end of my URL to which I'm sending this DELETE request
-    //         axios.delete("https://api.vschool.io/jonsmith/todo/" + singleTodoObject._id).then(function(response) {
-    //             // This made a DELETE request to "https://api.vschool.io/jonsmith/todo/5630dcfcac2dfab2428b8c02"
-    //             // Assuming I used the object from the example above.
-    //             alert("Your todo was successfully deleted!")
-    //         }, function(response) {
-    //             alert("There was a problem deleting your todo :(");
-    //         });
-    //     };
-
-// button.addEventListener("click", function(){
-//     axios.delete("https://api.vschool.io/scrimbalessons/todo/5d8bd511ee91575e6d49e06e")
-//         .then(response => console.log(response.data))
-//         .catch(error => console.log(error))
-// })
 
 // function listData(data){
 //     clearList()
@@ -84,7 +84,7 @@
 //     }
 // }
 
-getData()
+// getData()
 
 const todoForm = document["todo-form"]
 
@@ -108,5 +108,4 @@ todoForm.addEventListener("submit", function(e){
         .catch(err => console.log(err))
 
 })
-
 
