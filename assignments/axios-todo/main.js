1f40198caccfa898fa8828d5ae30b66ef3197746
Ally Nickell <allyjnickell@gmail.com>
2021-03-06 16:35:51 -0700

    delete function works- axios WIP

--- assignments/axios-todo/main.js
+++ assignments/axios-todo/main.js
@@ -1,7 +1,8 @@
-//FORM & POST
 const todoForm = document["todo-form"]
+
 todoForm.addEventListener("submit", function(e){
     e.preventDefault()
+
     const newTodo = {
         title: todoForm.title.value,
         price: todoForm.price.value,
@@ -13,29 +14,48 @@ todoForm.addEventListener("submit", function(e){
     todoForm.description.value = ""
     todoForm.imgURL.value = ""
     axios.post("https://api.vschool.io/[ally_n]/todo", newTodo)
-        .then(res => getData())
+        .then(
+            res => getData())
         .catch(err => console.log(err))
 })
 
 function getData(){
+    clearList()
     axios.get("https://api.vschool.io/[ally_n]/todo")
     .then(response => {
-        for(let i = 0; i < response.data.length; i++){
-            const h1 = document.createElement('h1')
+        for(let i = 0; i < response.data.length; i++){ 
+            getNew(response.data[i])   
+        }
+    })
+    .catch(error => console.log(error))
+    
+}
+getData()
+function clearList(){
+    const list = document.getElementById('todo-list')
+    while(list.firstChild){
+        list.removeChild(el.firstChild)
+    }
+}
+function autoRefresh( t ) {
+    setTimeout("location.reload(true);", t);
+ }
+function getNew(myNewResponse) {
+    const h1 = document.createElement('h1')
             const h2 = document.createElement('p')
             const p = document.createElement('p')
             const img = document.createElement('img')
-            const editBtn = document.createElement('BUTTON')
-            const delBtn = document.createElement('BUTTON')
-            var x = document.createElement("INPUT");
+            const editBtn = document.createElement('button')
+            const delBtn = document.createElement('button')
+            var x = document.createElement("input");
             x.className = 'checkbox';
             var label = document.createElement("label");
             x.setAttribute("type", "checkbox")
             x.setAttribute("name", "completed")
-            h1.textContent = response.data[i].title
-            h2.textContent = response.data[i].price
-            p.textContent = response.data[i].description
-            img.src = response.data[i].imgUrl
+            h1.textContent = myNewResponse.title
+            h2.textContent = myNewResponse.price
+            p.textContent = myNewResponse.description
+            img.src = myNewResponse.imgUrl
             img.width = 300
             editBtn.textContent = "Edit"
             delBtn.textContent = "Delete"
@@ -48,29 +68,46 @@ function getData(){
             document.getElementById('todo-list').appendChild(delBtn)
             document.getElementById('todo-list').appendChild(img)
             document.getElementById('todo-list').appendChild(editBtn)
-        }
-    })
-    .catch(error => console.log(error))
+
+           deleteTodo(delBtn, myNewResponse)
 }
-getData()
+function deleteTodo(deleteButton, data  ) {
+    deleteButton.addEventListener("click", function () {
+      axios.delete("https://api.vschool.io/[ally_n]/todo/" + data._id)
+        .then(response => {
+          deleteButton.parentNode.remove()
+          autoRefresh()
+          
+        })
+        .catch(error => console.log(error))
+    })
+ 
+  }
+// function deleteTodo(delBtn, myNewResponse) {
+//     delBtn.addEventListener("click", function() {
+//         console.log("button is working")
+//         axios.delete("https://api.vschool.io/[ally_n]/todo" + myNewResponse._id).then(function(response) {
+//             alert("Your todo was successfully deleted!")
+//         },
+//             alert("There was a problem deleting your todo."
+//     })
+    
+
+// deleteBtn.addEventListener("click", () => {
+//     axios.delete(`https://api.vschool.io/ragnarlothbrok/todo/${res.data[i]._id}`)
+//         .then(res => {
+//             document.getElementById("todo-items").innerHTML = ""
+//             getTodos()
+//         })
+//         .catch(err => console.log(err))
+// })
 
 
-    var deleteTodo = function(singleTodoObject) {
-        // This "singleTodoObject" I passed in has an attribute "_id" I can use to delete it
-        // I just need to add that "_id" to the end of my URL to which I'm sending this DELETE request
-            axios.delete("https://api.vschool.io/jonsmith/todo/" + singleTodoObject._id).then(function(response) {
-                // This made a DELETE request to "https://api.vschool.io/jonsmith/todo/5630dcfcac2dfab2428b8c02"
-                // Assuming I used the object from the example above.
-                alert("Your todo was successfully deleted!")
-            }, function(response) {
-                alert("There was a problem deleting your todo :(");
-            });
-        };
+
+    
 
 
 
-
-        
 // function listData(data){
 //     clearList()
     
@@ -88,12 +125,7 @@ getData()
 //     }
 // }
 
-// function clearList(){
-//     const list = document.getElementById('todo-list')
-//     while(list.firstChild){
-//         list.removeChild(el.firstChild)
-//     }
-// }
+
 
 
 
