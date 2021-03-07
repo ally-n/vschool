4081f676bf833a8bf7e402994a04390af8b4fcf2
Ally Nickell <allyjnickell@gmail.com>
2021-03-07 00:09:24 -0700

    completed level 2 assignments

--- assignments/axios-todo/main.js
+++ assignments/axios-todo/main.js
@@ -2,7 +2,6 @@ const todoForm = document["todo-form"]
 
 todoForm.addEventListener("submit", function(e){
     e.preventDefault()
-
     const newTodo = {
         title: todoForm.title.value,
         price: todoForm.price.value,
@@ -13,9 +12,10 @@ todoForm.addEventListener("submit", function(e){
     todoForm.price.value = ""
     todoForm.description.value = ""
     todoForm.imgURL.value = ""
+
     axios.post("https://api.vschool.io/[ally_n]/todo", newTodo)
         .then(
-            res => getData())
+            res => autoRefresh())
         .catch(err => console.log(err))
 })
 
@@ -27,108 +27,76 @@ function getData(){
             getNew(response.data[i])   
         }
     })
-    .catch(error => console.log(error))
-    
+    .catch(error => console.log(error)) 
 }
-getData()
+
 function clearList(){
     const list = document.getElementById('todo-list')
     while(list.firstChild){
         list.removeChild(el.firstChild)
     }
 }
-function autoRefresh( t ) {
+
+function autoRefresh(t){
     setTimeout("location.reload(true);", t);
- }
+    getData()
+}
+
 function getNew(myNewResponse) {
     const h1 = document.createElement('h1')
-            const h2 = document.createElement('p')
-            const p = document.createElement('p')
-            const img = document.createElement('img')
-            const editBtn = document.createElement('button')
-            const delBtn = document.createElement('button')
-            var x = document.createElement("input");
-            x.className = 'checkbox';
-            var label = document.createElement("label");
-            x.setAttribute("type", "checkbox")
-            x.setAttribute("name", "completed")
-            h1.textContent = myNewResponse.title
-            h2.textContent = myNewResponse.price
-            p.textContent = myNewResponse.description
-            img.src = myNewResponse.imgUrl
-            img.width = 300
-            editBtn.textContent = "Edit"
-            delBtn.textContent = "Delete"
-            document.getElementById('todo-list').style.padding.bottom = "100px";
-            document.getElementById('todo-list').appendChild(h1)
-            document.getElementById('todo-list').appendChild(h2)
-            document.getElementById('todo-list').appendChild(p)
-            document.getElementById('todo-list').appendChild(x)
-            document.getElementById('todo-list').appendChild(label)
-            document.getElementById('todo-list').appendChild(delBtn)
-            document.getElementById('todo-list').appendChild(img)
-            document.getElementById('todo-list').appendChild(editBtn)
-
-           deleteTodo(delBtn, myNewResponse)
+    const h2 = document.createElement('p')
+    const p = document.createElement('p')
+    const img = document.createElement('img')
+    const editBtn = document.createElement('button')
+    const delBtn = document.createElement('button')
+    const x = document.createElement("input");
+    const label = document.createElement("label");
+    h1.className = 'headerText'
+    x.className = 'checkbox';
+    x.setAttribute("type", "checkbox")
+    x.setAttribute("name", "completed")
+    h1.textContent = myNewResponse.title
+    h2.textContent = myNewResponse.price
+    p.textContent = myNewResponse.description
+    img.src = myNewResponse.imgUrl
+    img.width = 300
+    editBtn.textContent = "Edit"
+    delBtn.textContent = "Delete"
+    document.getElementById('todo-list').style.padding.bottom = "100px";
+    document.getElementById('todo-list').append(h1, h2, p, x, label, delBtn, img, editBtn)
+    deleteTodo(delBtn, myNewResponse)
+    completeTodo(x, myNewResponse)
+    if (myNewResponse.completed === true){
+    h1.style.textDecoration='line-through'
+    h2.style.textDecoration='line-through'
+    p.style.textDecoration='line-through'
+    x.checked = true;
+   }
 }
-function deleteTodo(deleteButton, data  ) {
+
+function deleteTodo(deleteButton, data) {
     deleteButton.addEventListener("click", function () {
       axios.delete("https://api.vschool.io/[ally_n]/todo/" + data._id)
         .then(response => {
           deleteButton.parentNode.remove()
           autoRefresh()
-          
         })
         .catch(error => console.log(error))
     })
- 
-  }
-// function deleteTodo(delBtn, myNewResponse) {
-//     delBtn.addEventListener("click", function() {
-//         console.log("button is working")
-//         axios.delete("https://api.vschool.io/[ally_n]/todo" + myNewResponse._id).then(function(response) {
-//             alert("Your todo was successfully deleted!")
-//         },
-//             alert("There was a problem deleting your todo."
-//     })
-    
+    }
 
-// deleteBtn.addEventListener("click", () => {
-//     axios.delete(`https://api.vschool.io/ragnarlothbrok/todo/${res.data[i]._id}`)
-//         .then(res => {
-//             document.getElementById("todo-items").innerHTML = ""
-//             getTodos()
-//         })
-//         .catch(err => console.log(err))
-// })
+function completeTodo(checkbox, data) {
+    checkbox.addEventListener("click", e => {        
+        if (checkbox.checked === true) {
+             axios.put("https://api.vschool.io/[ally_n]/todo/" + data._id, { completed: true })
+                .then(res => {
+                    console.log(res.data);
+                    autoRefresh()
+                }).catch(error => {
+                    console.log(error);
+                });
+        }
+    })
+}
 
-
-
-    
-
-
-
-// function listData(data){
-//     clearList()
-    
-//     for(let i = 0; i < data.length; i++){
-//         const h1 = document.createElement('h1')
-//         const h2 = document.createElement('h2')
-//         const p = document.createElement('p')
-//         // const h1 = document.createElement('h2') will be image URL
-//         h1.textContent = data[i].title
-//         h2.textContent = data[i].price
-//         p.textContent = data[i].description
-//         document.getElementById('todo-list').appendChild(h1)
-//         document.getElementById('todo-list').appendChild(h2)
-//         document.getElementById('todo-list').appendChild(p)
-//     }
-// }
-
-
-
-
-
-
-
-
+getData()
